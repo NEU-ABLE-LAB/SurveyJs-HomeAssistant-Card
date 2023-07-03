67e2ffbd64ec77986aa87dc401597e35203b5900
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-07-03 15:09:38 -0400

    nouislider css changes

--- css/nouislider.js
+++ css/nouislider.js
@@ -1,9 +1,6 @@
 import { css } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
 export const nouisliderStyles = css`
-  .noUi-touch-area {
-    background: lightgray;
-  }
   .noUi-target {
     margin-bottom: 20px !important;
     margin-top: -40px;
@@ -16,5 +13,20 @@ export const nouisliderStyles = css`
   }
   .noUi-marker-normal {
     display: none;
+  }
+  .noUi-horizontal {
+    height: 10px;
+  }
+  .noUi-handle {
+    border-radius: 50%;
+    background: lightblue;
+    box-shadow: none;
+    width: 20px !important;
+    height: 20px !important;
+    right: -8px !important;
+  }
+  .noUi-handle:after,
+  .noUi-handle:before {
+    background: none;
   }
 `;
