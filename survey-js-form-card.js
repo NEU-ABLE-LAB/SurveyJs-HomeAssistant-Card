23c5c5cbc944b057ce90443d56c2e5e12713d485
Maharshi-Pathak <64644720+Maharshi-Pathak@users.noreply.github.com>
2023-07-06 18:27:27 -0400

    changed to local path test

--- survey-js-form-card.js
+++ survey-js-form-card.js
@@ -4,8 +4,8 @@ import {
 } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 import "https://unpkg.com/nouislider/dist/nouislider.min.js";
 import "https://unpkg.com/jquery";
-import { nouisliderStyles } from "./css/nouislider.js";
-import { globalStyles } from "./css/global.js";
+import { nouisliderStyles } from "/local/css/nouislider.js";
+import { globalStyles } from "../css/global.js";
 
 class SurveyCard extends LitElement {
   static get properties() {
