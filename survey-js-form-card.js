8484d4ccb477b4587c1ca05fdf855a1d5b7bea11
Maharshi-Pathak <64644720+Maharshi-Pathak@users.noreply.github.com>
2023-07-12 14:15:47 -0400

    adds pointerchange event listener to sliders
    appends .color-change css attr to "noUi-handle" class

--- survey-js-form-card.js
+++ survey-js-form-card.js
@@ -208,6 +208,14 @@ class SurveyCard extends LitElement {
     $(this.shadowRoot.getElementById("surveyElement")).Survey({
       model: this.survey,
     });
+    // adds click handler 
+    const sliders = this.survey.getAllQuestionsByType('nouislider');
+    
+    sliders.forEach(function(slider) {
+    
+      slider.noUiSlider.on('pointerdown', function() {
+        slider.handleElement.classList.add('color-change');
+      });
   }
 
   pageCssLogic(options) {
